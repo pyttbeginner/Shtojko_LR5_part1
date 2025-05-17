e6d12db56e18c1d668c1c82ccf93dd9d74ffb4c6
pyttbeginner <110183548+pyttbeginner@users.noreply.github.com>
2025-05-17 11:37:43 +0300

    Update Shtojko_MathTask.cpp

--- Shtojko_MathTask.cpp
+++ Shtojko_MathTask.cpp
@@ -3,32 +3,20 @@
 
 using namespace std;
 
-int main()
-{
-    int numberA = 0, numberB = 0;
-    //enter variable
-    getNumber(numberA, "input height A");
-    //enter A
-    getNumber(numberB, "input height B");
+int main() {
+    int NumberA = 0, NumberB = 0;
 
-    getOperation(operation, "input operation to perform")
+    // Ввод переменной A
+    EnterDigit(NumberA, "input height A");
 
-    switch (operation) {
-            case '+':
-                result = add(num1, num2);
-                break;
-            case '-':
-                result = subtract(num1, num2);
-                break;
-            case '*':
-                result = multiply(num1, num2);
-                break;
-            case '/':
-                result = divide(num1, num2);
-                if (std::isnan(result)) {
-                    continue; // Пропускаем вывод результата при ошибке деления
-                }
-                break;
-cout << "Результат: " << num1 << " " << operation << " " << num2 << " = " << result << endl;
+    // Ввод переменной B
+    EnterDigit(NumberB, "input height B");
 
+    // Вычисление площади прямоугольника
+    int RectangleArea = CalcRectangleArea(NumberA, NumberB);
+
+    // Вывод значения площади
+    cout << "Area of Rectangle is " << RectangleArea << endl;
+
+    return 0;
 }
